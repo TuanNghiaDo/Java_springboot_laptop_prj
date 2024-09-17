9727b2444dc4752314ae2d10d595367524a9d27a
TuanNghiaDo <dotuannghia07@gmail.com>
2024-09-17 08:18:26 +0700

    video 58

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,12 +1,12 @@
 package vn.hoidanit.laptopshop.controller;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -19,26 +19,22 @@ public class UserController {
     }
 
     @RequestMapping("/")
-
     public String getHomePage(Model model) {
         String test = this.userService.handleHello();
-        model.addAttribute("Nghia", test);
+        model.addAttribute("eric", test);
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println(" run here " + hoidanit);
+        return "hello";
+    }
 }
-// @RestController
-// public class UserController {
-// // DI:Dependency Injection
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-
-// public String getHomePage() {
-// return this.userService.handleHello();
-// }
-// }
