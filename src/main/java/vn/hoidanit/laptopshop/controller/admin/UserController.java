384d0104d6248d72b9f802e6d35a26871f6c480a
TuanNghiaDo <dotuannghia07@gmail.com>
2024-09-30 13:16:46 +0700

    hashPassword

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -1,7 +1,10 @@
-package vn.hoidanit.laptopshop.controller;
+package vn.hoidanit.laptopshop.controller.admin;
 
 import java.util.List;
 
+import org.springframework.security.access.method.P;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +12,25 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import vn.hoidanit.laptopshop.domain.User;
-
+import vn.hoidanit.laptopshop.service.UploadService;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
 
     private final UserService userService;
+    private final UploadService uploadService;
+    private PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UploadService uploadService,
+            PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.uploadService = uploadService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     // Xem danh sách user
@@ -30,17 +39,20 @@ public class UserController {
         List<User> users = this.userService.getAllUsers();
         model.addAttribute("users1", users);
 
-        return "admin/user/table-user";
+        return "admin/user/show";
     }
 
     // xem chi tiết thông tin user
     @RequestMapping("/admin/user/{id}")
     public String getUserDetailPage(Model model, @PathVariable("id") Long id) {
-        System.out.println("check path id = " + id);
         model.addAttribute("id", id);
         User user = this.userService.getUserById(id);
         model.addAttribute("user", user);
-        return "admin/user/show"; // render ra file jsp
+        return "admin/user/detail";
+    }
+
+    public UserService getUserService() {
+        return userService;
     }
 
     // Home page
@@ -53,19 +65,24 @@ public class UserController {
         return "hello";
     }
 
-    // thêm mới user
-    @RequestMapping("/admin/user/create")
+    // thêm mới user, hiện form để nhập liệu
+    @GetMapping("/admin/user/create")
     public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
-        return "admin/user/create"; // render ra file jsp
+        return "admin/user/create";
     }
 
     // lưu user mới
-    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-        System.out.println(" run here " + hoidanit);
+    @PostMapping("/admin/user/create")
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit,
+            @RequestParam("hoidanitFile") MultipartFile file) {
+        String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
+        String hashPassword = this.passwordEncoder.encode(hoidanit.getPassword());
+        hoidanit.setAvatar(avatar);
+        hoidanit.setPassword(hashPassword);
+        hoidanit.setRole(this.userService.getRoleByName(hoidanit.getRole().getName()));
         this.userService.handleSaveUser(hoidanit);
-        return "redirect:/admin/user"; // render ra link URL
+        return "redirect:/admin/user";
     }
 
     // chuyển đến trang cập nhật user
--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,12 +1,17 @@
 package vn.hoidanit.laptopshop.domain;
 
-import jakarta.annotation.Generated;
+import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +22,39 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+
+    private String avatar;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
 
     public long getId() {
         return id;
